2da1d0b2881da308c6d7c87cf8badf8ea7236e6e
Joe Banks <joe@jb3.dev>
2021-12-02 03:24:34 +0000

    Add full stop to meta description

--- src/layouts/page.tsx
+++ src/layouts/page.tsx
@@ -22,7 +22,7 @@ interface PageProps {
 }
 
 export default function Page(props: PageProps) {
-    let description = props.description ? props.description : "Hey, I'm Joe! I'm a student at the University of Nottingham studying Computer Science and owner of Python Discord";
+    let description = props.description ? props.description : "Hey, I'm Joe! I'm a student at the University of Nottingham studying Computer Science and owner of Python Discord.";
     return (<div css={pageStyles}>
         <NavBar />
 
