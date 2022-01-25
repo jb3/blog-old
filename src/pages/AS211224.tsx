3c312ed906e9b3b4a935118a461e03a00bece9a3
Joe Banks <joe@jb3.dev>
2022-01-25 00:37:02 +0000

    Remove EVIX

--- src/pages/AS211224.tsx
+++ src/pages/AS211224.tsx
@@ -11,7 +11,7 @@ const AS211224 = () => {
       <p>AS211224 is a small IPv6-only education/research network run by Joe Banks.</p>
       <p>For more information, see the looking glass at <a href="https://lg.jb3.dev">lg.jb3.dev</a></p>
       <h3>Peering Policy</h3>
-      <p>In it's current form, AS211224 does not have many mutual peering points with other networks. You can find mutual points on PeeringDB <a href="https://www.peeringdb.com/net/27107">here</a>, as of now this is solely EVIX (routes <b>are</b> published to the EVIX route server). Please contact <a href="mailto:joe@jb3.dev">joe@jb3.dev</a> for peering questions, or find other contact points on PeeringDB.</p>
+      <p>In it's current form, AS211224 does not have many mutual peering points with other networks. You can find mutual points on PeeringDB <a href="https://www.peeringdb.com/net/27107">here</a>. Please contact <a href="mailto:joe@jb3.dev">joe@jb3.dev</a> for peering questions, or find other contact points on PeeringDB.</p>
     </Page>
   )
 }
